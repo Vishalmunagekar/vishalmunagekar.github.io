5f4d70da95e3439284bb76dfed581c52be07795c
munagvis <vishal.munagekar@tieto.com>
2021-11-06 12:34:10 +0530

    lots of chanes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let url = "";
 url = window.location.href;
-if(url.endsWith("index.html")){
+if(url.endsWith("/")){
     document.getElementById("home").classList.add('active');
 } else if(url.endsWith("profile.html")) {
     document.getElementById("blog").classList.add('active');
-} else if(url.endsWith("contact.html")) {
-    document.getElementById("contact").classList.add('active');
 }
+
+function sendEmail() { window.location.assign("mailto:vishalmunagekar@gmail.com"); }
