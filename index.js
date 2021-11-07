f470744047cf90c8fa0808c0b60f9b771e2dbd1d
munagvis <vishal.munagekar@tieto.com>
2021-11-07 11:16:16 +0530

    lots of chanes

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ url = window.location.href;
 if(url.endsWith("/")){
     document.getElementById("home").classList.add('active');
 } else if(url.endsWith("profile.html")) {
+    updateVisitorCount();
     document.getElementById("blog").classList.add('active');
 }
 
 function sendEmail() { window.location.assign("mailto:vishalmunagekar@gmail.com"); }
+
+const count = document.getElementById('count');
+
+function updateVisitorCount() {
+    fetch('https://api.countapi.xyz/update/vishal-munagekar/github.io/?amount=1')
+        .then(res => res.json())
+        .then(res => { count.innerHTML = res.value; });
+}
+
+// var option = {
+//     animation : true,
+//     autohide : true,
+//     delay : 5000
+// }
+// $('.toast').toast({delay : 50000});
+// $('.toast').toast('show');
